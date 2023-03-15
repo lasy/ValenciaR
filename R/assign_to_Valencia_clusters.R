8aa3978eb7a53f1e1b6dbe0b21cd0066b76d435b
lsymul <lsymul@stanford.edu>
2023-03-15 11:13:26 +0100

    add label_topics function

--- R/assign_to_Valencia_clusters.R
+++ R/assign_to_Valencia_clusters.R
@@ -33,7 +33,7 @@ assign_to_Valencia_clusters <- function(input, distance = "YC"){
     warning(
       str_c(
         "`input` does not have counts/proportions for all taxa represented in Valencia.\n",
-        "Data for ", length(missing_taxa),"/", nrow(v_clusters)," taxa are missing.\n",
+        "Data for ", length(missing_taxa),"/", ncol(v_clusters)," taxa are missing.\n",
         "These missing taxa represent around ",round(mean(missing_prop)*100),"% (",
         str_c(round(range(missing_prop)*100), collapse = "-"),"%) of Valencia cluster composition.\n",
         "Their list is in the `missing_taxa` component of the returned value."
@@ -43,8 +43,9 @@ assign_to_Valencia_clusters <- function(input, distance = "YC"){
 
   if (distance == "YC") {
     distances <- 1 - YC::YC(mat1 = input, mat2 = v_clusters)
-  }
-  else {
+  } else if (distance == "BC") {
+    distances <- BC(mat1 = input, mat2 = v_clusters)
+  } else {
     stop("Not yet implemented for other distances.\n")
   }
 
@@ -56,4 +57,53 @@ assign_to_Valencia_clusters <- function(input, distance = "YC"){
     assignment = data.frame(CST = CST, subCST = subCST) %>% set_rownames(rownames(input)),
     missing_taxa = missing_taxa
     )
+}
+
+
+#' Computes the Bray-Curtis dissimilarity between the rows of two matrices
+#'
+#' @param mat1 the matrix with the first set of samples
+#' @param mat2 (optional) the matric with the second set of samples
+#'
+#' @return a distance matrix whose dimensions are `nrow(mat1)` x `nrow(mat2)`
+#' @export
+#'
+BC <- function(mat1, mat2 = NULL) {
+  if (is.null(mat2))
+    mat2 <- mat1
+
+  # we harmonize columns
+  mat <-
+    bind_rows(
+      mat1 %>% as.data.frame(),
+      mat2 %>% as.data.frame()
+      ) %>%
+    as.matrix()
+  # we remove the NAs
+  mat[is.na(mat)] = 0
+
+  # re-convert to matrices
+  m1 <-
+    mat[1:nrow(mat1), ] %>%
+    matrix(nrow = nrow(mat1), ncol = ncol(mat))
+  m2 <-
+    mat[nrow(mat1) + (1:nrow(mat2)), ] %>%
+    matrix(nrow = nrow(mat2), ncol = ncol(mat))
+
+  # we compute the distance
+  # BC_ij = 1 - 2 * C_ij / (S_i + S_j)
+  # where C_ij = sum of the minimum values between i and j
+  dims <- c(nrow(m1), ncol(mat), nrow(m2))
+  M1 <- array(m1, dim = dims)
+  M2 <- array(rep(t(m2), each = nrow(m1)), dim = dims)
+  C <- apply(pmin(M1, M2), c(1, 3), sum)
+  S1 <- apply(M1, c(1,3), sum)
+  S2 <- apply(M2, c(1,3), sum)
+  D <- 1 - 2 * C / (S1 + S2)
+
+  # we relabel the rows and columns
+  rownames(D) <- rownames(mat1)
+  colnames(D) <- rownames(mat2)
+  D
+
 }
---
+++ R/label_topics.R
@@ -0,0 +1,108 @@
+
+
+#' Label topics to match the closest Valencia sub-CST
+#'
+#' @param lda_models a single LDA model (output of `topicmodels::LDA`) or
+#' a list of LDA models (e.g., output of `alto::fit_lda_models`).
+#' @param tax_table the taxonomic table for each row of the `beta` matrix
+#' @param distance the distance used to compute the (dis)similarity between the
+#' topic composition and the Valencia centroids.
+#'
+#' @return a single LDA model or a list of LDA models (depending on the input)
+#' where the rownames of `beta` and the colnames of `gamma` match the label
+#' of the closest Valencia sub-CST
+#' @export
+#'
+label_topics <- function(lda_models, tax_table, distance, max_distance = 0.5){
+
+  input <- lda_models
+  if (!is.null(lda_models$beta)) {
+    input <- list(unique_k = lda_models)
+  }
+
+  res <-
+    purrr::map(
+      .x = input,
+      .f = .label_topics_1_model,
+      tax_table = tax_table,
+      distance = distance,
+      max_distance = max_distance
+    )
+
+  if (!is.null(lda_models$beta)) {
+    res <- res[[1]]
+  }
+  res
+}
+
+
+#' Label topics from a single model to match the closest Valencia sub-CST
+#'
+#' @param lda_model a single LDA model (output of `topicmodels::LDA`).
+#' @param tax_table  the taxonomic table for each row of the `beta` matrix
+#' @param distance the distance used to compute the (dis)similarity between the
+#' topic composition and the Valencia centroids.
+#'
+#' @return a single LDA model where the rownames of `beta` and the colnames
+#' of `gamma` match the label of the closest Valencia sub-CST
+#'
+#' @importFrom stringr str_replace str_c
+#' @importFrom dplyr tibble mutate case_when group_by ungroup n
+.label_topics_1_model <-
+  function(lda_model, tax_table, distance, max_distance){
+
+    beta_Valencia_tax <-
+      convert_to_Valencia_taxonomy(
+        lda_model$beta %>% exp(),
+        tax_table = tax_table
+      )
+
+    clusters <-
+      assign_to_Valencia_clusters(
+        beta_Valencia_tax$converted_input,
+        distance = distance
+      )
+
+    tmp <-
+      tibble(
+        topic_name =
+          clusters$assignment$subCST %>%
+          stringr::str_replace("I-[A-B]","I"),
+        CST = clusters$assignment$CST,
+        distance = clusters$distances %>% apply(., 1, min)
+      ) %>%
+      mutate(
+        topic_name =
+          case_when(
+            distance > max_distance ~ str_c(CST, "-O"),
+            TRUE ~ topic_name
+          )
+      )
+
+    topic_names <- tmp$topic_name
+
+    if (any(duplicated(topic_names))) {
+      tmp <-
+        tmp %>%
+        group_by(topic_name) %>%
+        mutate(
+          n = rank(distance),
+          N = n(),
+          topic_name_suffix =
+            case_when(
+              N == 1 ~ "",
+              N > 1 ~ str_c(".",letters[n])
+            ),
+          new_topic_name =
+            str_c(topic_name, topic_name_suffix)
+        )  %>%
+        dplyr::ungroup()
+      topic_names <-  tmp$new_topic_name
+    }
+
+    rownames(lda_model$beta) <- topic_names
+    colnames(lda_model$gamma) <- topic_names
+
+    lda_model
+
+  }
